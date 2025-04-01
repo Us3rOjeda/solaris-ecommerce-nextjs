f77e0b9c84c494550ca3e38729957473f430a602
us3rOjeda <189202027+Us3rOjeda@users.noreply.github.com>
2025-04-01 04:42:12 -0300

    add fontPlay

--- public/assets/fonts/fonts.ts
+++ public/assets/fonts/fonts.ts
@@ -1,3 +1,7 @@
-import { Play } from 'next/font/google'
+import { Play } from 'next/font/google';
 
-export const fontPlay = Play({weight: ['400']})
+export const fontPlay = Play({
+  subsets: ['latin'], 
+  weight: '400',
+  style: 'normal',
+});
